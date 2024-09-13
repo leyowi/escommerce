926173a61840dbbc82a088d921a17e4aa48be591
leyowi <anthonyleouiepalmera310@gmail.com>
2024-09-13 11:48:55 +0800

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ESCO D</title>
+
+    <link rel="stylesheet" href="home-style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
+</head>
+
+<body class="pt-5">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+        <div class="container">
+            <div class="col">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/escod.png" 
+                    width="70" height="30" class="d-inline-block align-top" alt="Company Logo">
+                </a>
+            </div>
+
+            <div class="col-8">
+                <div class="collapse navbar-collapse justify-content-center">
+                    
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Aver</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Elmo</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nureva</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Visionary</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col text-end">
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <i class="bi bi-cart3 text-secondary d-none d-md-block" style="font-size: 24px;"></i>
+
+            </div>
+        </div>
+    </nav>
+    
+    <div class="offcanvas offcanvas-end text-bg-dark w-" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">ESCO D</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+
+        <div class="offcanvas-body">
+
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Aver</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Elmo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nureva</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Visionary</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My Cart</a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="container text-white text-end">
+            <div class="row">
+                <div class="col-12">
+                    <span class="Title-text d-flex justify-content-end">
+                        Find your devices...
+                    </span>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-end">
+                <div class="col-8 mb-3">
+                    <span class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nulla molestias, officiis architecto voluptas modi ad ullam repellendus ex veniam? Vel ipsa eveniet voluptate ipsum, beatae eos nihil commodi esse!</span>
+                </div>
+            </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+    
+</html>
